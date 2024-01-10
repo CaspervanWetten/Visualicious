5f7c433bdc6773c81742c7254f0c0b7aa7131338
Casper van Wetten <c.o.vanwetten@gmail.com>
2024-01-10 16:25:01 +0100

    een jaarlijkse optelling van misdrijven in utrecht voor bar chart-3

--- js/bar-chart-3.js
+++ js/bar-chart-3.js
@@ -1,21 +1,47 @@
+function responsivefy(svg) {
+  // get container + svg aspect ratio
+  var container = d3.select(svg.node().parentNode),
+    width = parseInt(svg.style("width")),
+    height = parseInt(svg.style("height")),
+    aspect = width / height;
+
+  // add viewBox and preserveAspectRatio properties,
+  // and call resize so that svg resizes on inital page load
+  svg
+    .attr("viewBox", "0 0 " + width + " " + height)
+    .attr("perserveAspectRatio", "xMinYMid")
+    .call(resize);
+
+  // to register multiple listeners for same event type,
+  // you need to add namespace, i.e., 'click.foo'
+  // necessary if you call invoke this function for multiple svgs
+  // api docs: https://github.com/mbostock/d3/wiki/Selections#on
+  d3.select(window).on("resize." + container.attr("id"), resize);
+
+  // get width of container and resize svg to fit it
+  function resize() {
+    var targetWidth = parseInt(container.style("width"));
+    svg.attr("width", targetWidth);
+    svg.attr("height", Math.round(targetWidth / aspect));
+  }
+}
+
 function functieTotaalMisdrijvenUtrecht(data) {
   const margin = { top: 20, right: 20, bottom: 50, left: 50 };
   const width = 600 - margin.left - margin.right;
-  const height = 400 - margin.top - margin.bottom;
+  const height = 300 - margin.top - margin.bottom;
 
-  // SVG element
   const svg = d3
     .select("#totaalMisdrijvenUtrecht")
     .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("viewBox", `0 0 600 300`)
     .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+    .attr("transform", `translate(${margin.left},${margin.top})`)
+    .call(responsivefy);
 
-  // X and Y scales
   const x = d3
     .scaleBand()
-    .domain(data.map((d) => d.periode))
+    .domain(data.map((d) => d.Perioden))
     .range([0, width])
     .padding(0.1);
 
@@ -25,53 +51,27 @@ function functieTotaalMisdrijvenUtrecht(data) {
     .nice()
     .range([height, 0]);
 
-  const xAxis = d3.axisBottom(x);
-  const yAxis = d3.axisLeft(y);
-
   svg
     .append("g")
-    .attr("transform", `translate(0,${height})`)
-    .call(xAxis)
-    .append("text")
-    .attr("x", width / 2)
-    .attr("y", margin.bottom - 10)
-    .attr("fill", "#000")
-    .attr("text-anchor", "middle")
-    .text("Tijdsperiode");
+    .attr("transform", `translate(0, ${height})`)
+    .call(d3.axisBottom(x));
 
-  // Append y-axis to SVG and add label
-  svg
-    .append("g")
-    .call(yAxis)
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", -25)
-    .attr("x", -(height / 2))
-    .attr("dy", "-1em")
-    .attr("fill", "#000")
-    .attr("text-anchor", "middle")
-    .text("Aantal misdrijven");
+  svg.append("g").call(d3.axisLeft(y));
 
-  svg.append("g").attr("transform", `translate(0,${height})`).call(xAxis);
-  svg.append("g").call(yAxis);
-
-  // Create bars
   svg
     .selectAll(".bar")
     .data(data)
     .enter()
     .append("rect")
     .attr("class", "bar")
-    .attr("x", (d) => x(d.periode))
+    .attr("x", (d) => x(d.Perioden))
     .attr("width", x.bandwidth())
     .attr("y", (d) => y(+d.aantal))
-    .attr("height", (d) => height - y(+d.aantal))
-    .attr("fill", "steelblue");
+    .attr("height", (d) => height - y(+d.aantal));
 }
 
 d3.csv("scraper/archive/totaalMisdrijvenUtrecht.csv").then(
   (totaalMisdrijvenUtrecht) => {
-    console.log(totaalMisdrijvenUtrecht);
     functieTotaalMisdrijvenUtrecht(totaalMisdrijvenUtrecht);
   }
 );
